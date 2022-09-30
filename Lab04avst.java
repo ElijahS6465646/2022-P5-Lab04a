83f56e187f39bfaa6c89699edd06e7a928a9d568
Elijah Suarez <elijah.suarez@vtsd.com>
2022-09-30 13:11:41 -0400

    80 point version Working on 90 point version

--- Lab04avst.java
+++ Lab04avst.java
@@ -12,16 +12,21 @@ public class Lab04avst
         double principal  = 250000;
         double annualRate = 4.85;
         double numYears   = 30;
+        double numMonths = numYears* 12;
+        double monthlyRate = (annualRate/100) / 12;
+        double numerator= monthlyRate * Math.pow(1+monthlyRate,numMonths);
+        double denominator = Math.pow(1+monthlyRate,numMonths) - 1;
 
 
 
 
 
+        System.out.println("Principal: \t\t " + principal);
+        System.out.println("Annual Rate: \t " + annualRate);
+        System.out.println("Number of years: " + numYears);
+        System.out.println("Monthly Payment: " + numerator/denominator * principal );
+        System.out.println("Total Payments   " + (numerator/denominator * principal) * numMonths);
 
-
-
-
-        System.out.println();
     }
 }
 
